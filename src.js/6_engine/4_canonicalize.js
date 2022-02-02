3447bd1af37a6f0ae200697e67ae879ef7393364
itozyun <itozyun@gmail.com>
2022-02-03 08:03:24 +0900

    (a)param の修正

--- src.js/6_engine/4_canonicalize.js
+++ src.js/6_engine/4_canonicalize.js
@@ -1,7 +1,7 @@
 /** Return case-folded code point for ignore-case comparison.
  *
  * @param {number} c
- * @param {boolean|undefined} unicode
+ * @param {boolean=} unicode
  * @return {number} 
  */
 canonicalize = function( c, unicode ){
@@ -27,7 +27,7 @@ canonicalize = function( c, unicode ){
  * It is used for character class matching on ignore-case.
  *
  * @param {number} c
- * @param {boolean|undefined} unicode
+ * @param {boolean=} unicode
  * @return {Array.<number>} 
  */
 uncanonicalize = function( c, unicode ){
